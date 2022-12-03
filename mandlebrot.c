c8beaa3edaec97c29d22bd91d46529f744a82d91
Louis Warren <louis@lsw.nz>
2022-12-04 10:09:48 +1300

    Allocate workspace up front

--- mandlebrot.c
+++ mandlebrot.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <complex.h>
 
 #include <omp.h>
+#include <assert.h>
 
 #define die(...) do { \
 		fprintf(stderr, __VA_ARGS__); \
 		fprintf(stderr, "\n"); \
 		exit(1); \
 	} while(0)
+
+struct workspace {
+	int width, height;
+	int n;
+	void *z_arr;
+	void *esc_n;
+};
 
 inline
 static
@@ -25,42 +34,82 @@ escaped(double complex z)
 	return a * a + b * b > 4;
 }
 
-void
-render(
-	int height, int steps,
-	double xmin, double xmax, double ymin, double ymax
-) {
-	int width = height * (xmax - xmin) / (ymax - ymin) + 0.5;
-
+int
+workspace_init(struct workspace *w, int width, int height)
+{
 	complex double (*zarr)[height] = calloc(width, sizeof(*zarr));
-	int (*inds)[height] = calloc(width, sizeof(*inds));
+	unsigned int (*inds)[height] = calloc(width, sizeof(*inds));
+	w->width = width;
+	w->height = height;
+	w->n = 0;
+	w->z_arr = zarr;
+	w->esc_n = inds;
+	return !w->z_arr || !w->esc_n;
+}
 
-	if (!zarr || !inds)
-		die("calloc failed");
+void
+workspace_reset(struct workspace *w)
+{
+	w->n = 0;
+	memset(w->z_arr, 0, w->width * w->height * sizeof(w->z_arr));
+	memset(w->esc_n, 0, w->width * w->height * sizeof(w->esc_n));
+}
 
-	printf("P2\n%d %d\n%d\n", width, height, steps);
+/* I probably won't need this */
+void
+workspace_deinit(struct workspace *w)
+{
+	w->width = 0;
+	w->height = 0;
+	w->n = 0;
+	free(w->z_arr);
+	free(w->esc_n);
+}
 
-	for (int n = 0; n < steps; ++n) {
-		for (int j = 0; j < height; ++j) {
-			#pragma omp parallel for
-			for (int i = 0; i < width; ++i) {
-				double a = xmin + (xmax - xmin) * i / width;
-				double b = ymax - (ymax - ymin) * j / height;
-				zarr[i][j] = m(zarr[i][j], a + I * b);
-				inds[i][j] |= !inds[i][j] * escaped(zarr[i][j]) * n;
-			}
-		}
-	}
+void
+output(const struct workspace *w)
+{
+	unsigned int (*inds)[w->height] = w->esc_n;
 
-	for (int j = 0; j < height; ++j) {
-		for (int i = 0; i < width; ++i) {
+	printf("P2\n%d %d\n%d\n", w->width, w->height, w->n);
+	for (int j = 0; j < w->height; ++j) {
+		for (int i = 0; i < w->width; ++i) {
 			printf("%d ", inds[i][j]);
 		}
 		putchar('\n');
 	}
 
-	free(zarr);
-	free(inds);
+}
+
+void
+render_once(
+	struct workspace *w,
+	double xmin, double xmax, double ymin, double ymax
+) {
+	complex double (*zarr)[w->height] = w->z_arr;
+	unsigned int (*inds)[w->height] = w->esc_n;
+
+	for (int j = 0; j < w->height; ++j) {
+		#pragma omp parallel for
+		for (int i = 0; i < w->width; ++i) {
+			double a = xmin + (xmax - xmin) * i / w->width;
+			double b = ymax - (ymax - ymin) * j / w->height;
+			zarr[i][j] = m(zarr[i][j], a + I * b);
+			inds[i][j] |= !inds[i][j] * escaped(zarr[i][j]) * w->n;
+		}
+	}
+	w->n++;
+}
+
+void
+render(
+	struct workspace *w,
+	int steps,
+	double xmin, double xmax, double ymin, double ymax
+) {
+	for (int n = 0; n < steps; ++n) {
+		render_once(w, xmin, xmax, ymin, ymax);
+	}
 }
 
 int
@@ -71,8 +120,17 @@ main(int argc, char *argv[])
 	double ymin = -1.25;
 	double ymax = 1.25;
 
+	int height = 1080;
+	int width = height * (xmax - xmin) / (ymax - ymin) + 0.5;
+	assert(width == height);
+
+	struct workspace w;
+	if (workspace_init(&w, width, height))
+		die("Failed to allocate %dx%d workspace", width, height);
+
 	if (argc == 1) {
-		render(1080, 100, xmin, xmax, ymin, ymax);
+		render(&w, 100, xmin, xmax, ymin, ymax);
+		output(&w);
 	}
 	else if (argc > 1 && argv[1][0] == 'z') {
 		// Zoom towards (-1.40065, 0)
@@ -83,7 +141,9 @@ main(int argc, char *argv[])
 			xmax -= 0.01 * (1 - ratio);
 			ymin += 0.005;
 			ymax -= 0.005;
-			render(1080, 20, xmin, xmax, ymin, ymax);
+			render(&w, 20, xmin, xmax, ymin, ymax);
+			workspace_reset(&w);
+			output(&w);
 		}
 	}
 }
